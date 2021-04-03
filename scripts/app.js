f04b92d188d1ed2d6a4eaae5fb014e3bcef9da30
Suleika Tentua <suleika_t@hotmail.com>
2021-04-03 11:22:03 +0200

    eindopdracht finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-//functie define url 
+//function to define url 
 const getUrl = (imdbID) => {
     let movieListLink = document.createElement('a');
     movieListLink.href = 'https://www.imdb.com/title/' + imdbID;
@@ -17,18 +17,17 @@ const titleMapper = (movie) => {
     movieListLink.appendChild(movieListImg);
     movieListItem.appendChild(movieListLink);
     return movieListItem;
-}
+};
 
 
 const addMoviesToDom = (movies) => {
     let movieList = document.getElementById('movie-list');
     movieList.querySelectorAll('*').forEach(element => element.remove());
-    // console.log("Movie list:", movieList);
     let movieTitles = movies.map(titleMapper);
     movieTitles.forEach(element => {
         movieList.appendChild(element);
     });
-}
+};
 
 
 // Function to filter on specific genre/word
@@ -39,47 +38,73 @@ const filterMovies = (wordInMovieTitle) => {
     return filtered;
 };
 
+
 //Function to filter the search form
 const searchMovies = (event) => {
     event.preventDefault();
     let searchInput = document.querySelector('.search-input');
-    let searchedMovies = filterMovies(searchInput.value);
+    let searchedMovies = [];
+    if (searchInput.value.toUpperCase() === 'new'.toUpperCase()) { 
+        searchedMovies = filterLatestMovies();
+    } else {
+        searchedMovies = filterMovies(searchInput.value);
+    }
     if (searchedMovies.length == 0) {
-        return document.getElementById('movie-list').innerHTML = `<h2>Sorry we could not find any matching movies, please try again!<h2>`;
+        let movieList = document.getElementById('movie-list');
+        movieList.querySelectorAll('*').forEach(element => element.remove());
+        let h2 = document.createElement('h2');
+        h2.innerHTML = "Sorry we could not find any matching movies, please try again!";
+        h2.setAttribute('class', 'h2');
+        movieList.appendChild(h2);
+        setHeaderText('');
     } 
-    addMoviesToDom(searchedMovies);
-}
+    else {
+        setHeaderText('Choose one of your favorite movies containing: ' + searchInput.value);
+        addMoviesToDom(searchedMovies);
+    }
+};
+
 
 // function to filter latestMovies 
 const filterLatestMovies = () => {
     let filtered = movies.filter(element => {
         return element.Year >= 2014; 
     });
-    //console.log(filtered);
     return filtered;
-}
+};
+
+
+// function to update text when filtering
+const setHeaderText = (title) => {
+    let titleElement = document.querySelector('.movie-title');
+    titleElement.innerHTML = title;
+};
+
 
 // Adding the filter function to every individual radio button
 const handleOnChangeEvent = event => { 
     let radioBtnValue = event.target.value;
     let filteredMovies = [];
-    // console.log('Pressed on radio button:', event.target.value);
         switch (radioBtnValue) {
         case ('new'):
             filteredMovies = filterLatestMovies();
+            setHeaderText('Choose one of the newest movies!');
             break;
         case 'avengers':
             filteredMovies = filterMovies('Avengers');
+            setHeaderText('Choose one of your favorite Avengers movie!');
             break;
         case ('x-men'):
             filteredMovies = filterMovies ('X-Men');
+            setHeaderText('Choose one of your favorite X-men movie!');
             break;
         case ('princess'):
             filteredMovies = filterMovies ('Princess');
+            setHeaderText('Choose one of your favorite Princess movie!');
             break;
         case ('batman'):
-            let hoi = 'Batman'
             filteredMovies = filterMovies ('Batman');
+            setHeaderText('Choose one of your favorite Batman movie!');
             break;
         }
         addMoviesToDom(filteredMovies);
@@ -96,6 +121,7 @@ const addEventListeners = () => {
     searchForm.addEventListener('submit', searchMovies);
 };
 
+
 // mobile website menu toggle 
 const burgerSlide = () => {
     let burger = document.querySelector('.burger-btn');
@@ -104,12 +130,13 @@ const burgerSlide = () => {
     burger.addEventListener('click', () => {
       navBarToggle.classList.toggle('radio-active');
     });
-}
+};
+
 
 const app = () => {
     addEventListeners();
     burgerSlide();
     addMoviesToDom(movies);
-}
+};
 
 app();
